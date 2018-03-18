b7525460127acff71e8c5b74cce00700373b4214
Ian <iang4llmeister@gmail.com>
2018-03-17 21:06:17 -0700

    Add handling for ~

--- make_absolute.py
+++ make_absolute.py
@@ -9,6 +9,7 @@ import re
 
 def make_absolute( path ):
   if os.name == 'nt':
+    pass
     #cd \
     #Starts with '\' -- '.\\' in python
     #Starts with '.\'
@@ -18,8 +19,11 @@ def make_absolute( path ):
     if path[0] == '/':
       pass
     #'~/'
-    elif path[:2] == '~/':
-      path = os.environ['HOME'] + path[1:]
+    elif path[:1] == '~':
+      if path[:2] == '~/' or len(path) == 1: #My home
+        path = os.path.join( os.path.expanduser('~'), path[1:] )
+      else: #Someone else's home
+        path = os.sep.join( [ os.path.expanduser(path.split('/')[0]) ] + path.split('/')[1:] )
     #Starts with './'
     elif path[:2] == './':
       path = os.path.join( os.getcwd(), path[2:] )
